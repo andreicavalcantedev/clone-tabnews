3ed42a1df77946e2e0b85d31779a6fe3999bc38f
Andrei Cavalcante <andreisilva96832@gmail.com>
2025-03-13 11:13:24 -0300

    adding scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Esse é um projeto para praticar e aprender novas skills fazendo a implementação do https://www.tabnews.com.br/ do ZERO.
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,23 @@
 function HomeScreen() {
+  const dayOfTheWeek = new Date().getDay();
 
-    const dayOfTheWeek = new Date().getDay()
+  const daysOfTheWeekNames = {
+    0: "Domingo",
+    1: "Segunda-feira",
+    2: "Terça-feira",
+    3: "Quarta-feira",
+    4: "Quinta-feira",
+    5: "Sexta-feira",
+    6: "Sábado",
+  };
 
-    const daysOfTheWeekNames = {
-        0: 'Domingo',
-        1: 'Segunda-feira',
-        2: 'Terça-feira',
-        3: 'Quarta-feira',
-        4: 'Quinta-feira',
-        5: 'Sexta-feira',
-        6: 'Sábado'
-    }
+  function daysOfTheWeekInString(dayOfTheWeek) {
+    return (
+      String(daysOfTheWeekNames[dayOfTheWeek]) || "Não sei que dia é esse... :("
+    );
+  }
 
-    function daysOfTheWeekInString (dayOfTheWeek) {
-        return String(daysOfTheWeekNames[dayOfTheWeek]) || 'Não sei que dia é esse... :('
-    }
-
- return (<h1>Oi! Hoje é: {daysOfTheWeekInString(dayOfTheWeek)}</h1>)
+  return <h1>Oi! Hoje é: {daysOfTheWeekInString(dayOfTheWeek)}</h1>;
 }
 
-export default HomeScreen
+export default HomeScreen;
